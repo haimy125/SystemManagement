6b917b1df4199957b046b60a71dc12aec14b87f0
myph <haimyphan125@gmail.com>
2024-05-16 17:16:57 +0700

    first commit

---
+++ SystemManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace SystemManagement
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Dùng bảng mã UTF8 nếu các nội dung có dấu*/
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Hello world");
+        }
+    }
+}
