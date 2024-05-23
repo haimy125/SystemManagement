e488bb03eb7c3bb06b2325d8457c847877c8d0a3
myph <haimyphan125@gmail.com>
2024-05-23 15:36:02 +0700

    thêm thuộc tính Lương

--- SystemManagement/Models/EmployeeModel.cs
+++ SystemManagement/Models/EmployeeModel.cs
@@ -12,14 +12,20 @@ namespace SystemManagement.Models
     public enum Position
     {
         Teacher,
-        Staff
+        Manager,
+        Developer,
+        Designer,
+        Tester,
+        HR,
+        Other
     }
-
+    
     public class EmployeeModel
     {
         public int EmployeeId { get; set; }
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public decimal Salary { get; set; }
         public Gender Gender { get; set; }
         public Position Position { get; set; }
         public DepartmentModel Department { get; set; }
